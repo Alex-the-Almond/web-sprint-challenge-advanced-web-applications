e53519ba21a55dd22510c3ab60ad83528920827e
Alexander Aymond <lynxkhaos@gmail.com>
2022-06-28 16:15:54 -0500

    working on last bug

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,9 +5,8 @@ import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import * as goFetch from '../fetch'
 
-const articlesUrl = 'http://localhost:9000/api/articles'
-const loginUrl = 'http://localhost:9000/api/login'
 
 export default function App() {
   // ✨ MVP can be achieved with these states
@@ -16,60 +15,87 @@ export default function App() {
   const [currentArticleId, setCurrentArticleId] = useState()
   const [spinnerOn, setSpinnerOn] = useState(false)
 
-  // ✨ Research `useNavigate` in React Router v.6
+  const token = localStorage.getItem('token');
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => {navigate('/')}
+  const redirectToArticles = () => {navigate('/articles')}
 
   const logout = () => {
-    // ✨ implement
-    // If a token is in local storage it should be removed,
-    // and a message saying "Goodbye!" should be set in its proper state.
-    // In any case, we should redirect the browser back to the login screen,
-    // using the helper above.
+    if (localStorage.getItem('token')) {
+      localStorage.removeItem('token');
+      setMessage('Goodbye!')
+      redirectToLogin();
+    }
   }
 
   const login = ({ username, password }) => {
-    // ✨ implement
-    // We should flush the message state, turn on the spinner
-    // and launch a request to the proper endpoint.
-    // On success, we should set the token to local storage in a 'token' key,
-    // put the server success message in its proper state, and redirect
-    // to the Articles screen. Don't forget to turn off the spinner!
-  }
+      setCurrentArticleId();
+      setMessage('');
+      setSpinnerOn(true);
+      goFetch.login({username, password})
+      .then(res => {
+        setMessage(res.message);
+        setSpinnerOn(false);
+        redirectToArticles();
+  })
+}
 
-  const getArticles = () => {
-    // ✨ implement
-    // We should flush the message state, turn on the spinner
-    // and launch an authenticated request to the proper endpoint.
-    // On success, we should set the articles in their proper state and
-    // put the server success message in its proper state.
-    // If something goes wrong, check the status of the response:
-    // if it's a 401 the token might have gone bad, and we should redirect to login.
-    // Don't forget to turn off the spinner!
+const getArticles = () => {
+  setMessage('');
+  setSpinnerOn(true);
+  goFetch.getArticles(token)
+    .then(res => {
+    if (res.message) {
+    setArticles(res.articles);
+    setMessage(res.message);
+    } else if (res === 401) {
+      console.error("Resubmit for a token!");
+      redirectToLogin();
+    }
+    setSpinnerOn(false);
+  })
+    .catch(err => console.error(err));
   }
 
   const postArticle = article => {
-    // ✨ implement
-    // The flow is very similar to the `getArticles` function.
-    // You'll know what to do! Use log statements or breakpoints
-    // to inspect the response from the server.
+    setSpinnerOn(true);
+    goFetch.addArticle(token, article)
+    .then(res => {
+      setArticles([...articles, res.article]);
+      setMessage(res.message);
+      setSpinnerOn(false);
+    })
   }
 
-  const updateArticle = ({ article_id, article }) => {
-    // ✨ implement
-    // You got this!
+  const updateArticle = ( article_id, article ) => {
+    setSpinnerOn(true);
+    goFetch.editArticle(token, article, article_id)
+    .then(res => {
+      setArticles([
+        ...articles.filter(item => item.article_id !== article_id), res.article
+      ]);
+    
+      setMessage(res.message);
+      setSpinnerOn(false);
+    });
   }
 
   const deleteArticle = article_id => {
-    // ✨ implement
+    setSpinnerOn(true);
+    goFetch.deleteArticle(token, article_id)
+    .then(res => {
+      setArticles([
+        ...articles.filter(item => item.article_id !== article_id)
+      ]);
+      setMessage(res.message);
+      setSpinnerOn(false);
+    })
   }
 
   return (
-    // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
-    <>
-      <Spinner />
-      <Message />
+    <React.StrictMode>
+      <Spinner spinnerOn={spinnerOn} />
+      <Message message={message}/>
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,16 +104,28 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login}/>} />
           <Route path="articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm 
+              currentArticleId={currentArticleId}
+              updateArticle={updateArticle}
+              postArticle={postArticle}
+              setCurrentArticleId={setCurrentArticleId}
+
+              articles={articles}/>
+              <Articles getArticles={getArticles} 
+              articles={articles} 
+              redirectToLogin={redirectToLogin} 
+              token={token}
+              setCurrentArticleId={setCurrentArticleId}
+              deleteArticle={deleteArticle}
+              />
             </>
           } />
         </Routes>
         <footer>Bloom Institute of Technology 2022</footer>
       </div>
-    </>
+    </React.StrictMode>
   )
 }
--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -5,14 +5,14 @@ const initialFormValues = { title: '', text: '', topic: '' }
 
 export default function ArticleForm(props) {
   const [values, setValues] = useState(initialFormValues)
-  // ✨ where are my props? Destructure them here
-
+  
+  const {articles, setCurrentArticleId, currentArticleId, updateArticle, postArticle} = props;
   useEffect(() => {
-    // ✨ implement
-    // Every time the `currentArticle` prop changes, we should check it for truthiness:
-    // if it's truthy, we should set its title, text and topic into the corresponding
-    // values of the form. If it's not, we should reset the form back to initial values.
-  })
+    currentArticleId ? 
+    setValues(articles.find(val => val.article_id === currentArticleId)) :
+    setValues(initialFormValues);
+    
+  }, [currentArticleId])
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -20,20 +20,21 @@ export default function ArticleForm(props) {
   }
 
   const onSubmit = evt => {
-    evt.preventDefault()
-    // ✨ implement
-    // We must submit a new post or update an existing one,
-    // depending on the truthyness of the `currentArticle` prop.
+    evt.preventDefault();
+    currentArticleId ? 
+    updateArticle(currentArticleId, values) :
+    postArticle(values);
+    setValues(initialFormValues);
+    
   }
 
   const isDisabled = () => {
-    // ✨ implement
-    // Make sure the inputs have some values
+    const {text, title, topic} = values;
+    return !text || !title || !topic;
+   
   }
 
   return (
-    // ✨ fix the JSX: make the heading display either "Edit" or "Create"
-    // and replace Function.prototype with the correct function
     <form id="form" onSubmit={onSubmit}>
       <h2>Create Article</h2>
       <input
@@ -58,7 +59,7 @@ export default function ArticleForm(props) {
       </select>
       <div className="button-group">
         <button disabled={isDisabled()} id="submitArticle">Submit</button>
-        <button onClick={Function.prototype}>Cancel edit</button>
+        <button onClick={() => setCurrentArticleId(null)}>Cancel edit</button>
       </div>
     </form>
   )
